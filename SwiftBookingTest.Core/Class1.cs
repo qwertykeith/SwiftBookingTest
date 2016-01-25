832bb5f143d3c905c5ad3bb75fdc2558fd239a93
atul221282 <atul221282@gmail.com>
2016-01-26 08:45:00 +1030

    added core project

---
+++ SwiftBookingTest.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftBookingTest.Core
+{
+    public class Class1
+    {
+    }
+}
