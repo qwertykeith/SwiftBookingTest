1309e93bc8aec20f0d5b08ff3f942381597a1693
Alastair Pitts <alastair@pitts.io>
2016-01-30 14:06:27 +1100

    Update packages and started designing the page
    Updated all available NuGet packages
    Designed initial data model
    Designed the page using bootstrap & angular

---
+++ SwiftBookingTest.Web/Api/ClientsController.cs
@@ -0,0 +1,119 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SwiftBookingTest.Data;
+using SwiftBookingTest.Data.Model;
+
+namespace SwiftBookingTest.Web.Api
+{
+    [RoutePrefix("api/Clients")]
+    public class ClientsController : ApiController
+    {
+        private SwiftBookingContext db = new SwiftBookingContext();
+
+        // GET: api/Clients
+        public IQueryable<Client> GetClients()
+        {
+            return db.Clients;
+        }
+
+        // GET: api/Clients/5
+        [ResponseType(typeof(Client))]
+        public async Task<IHttpActionResult> GetClient(int id)
+        {
+            Client client = await db.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+
+        // PUT: api/Clients/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutClient(int id, Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != client.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Clients
+        [ResponseType(typeof(Client))]
+        public async Task<IHttpActionResult> PostClient(Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Clients.Add(client);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = client.Id }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [ResponseType(typeof(Client))]
+        public async Task<IHttpActionResult> DeleteClient(int id)
+        {
+            Client client = await db.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            db.Clients.Remove(client);
+            await db.SaveChangesAsync();
+
+            return Ok(client);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClientExists(int id)
+        {
+            //this is a safer way of checking if the client exists.
+            //Using .Count() is inefficient compared to .Any()
+            return db.Clients.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SwiftBookingTest.Web/app/controllers/BookingController.js
@@ -0,0 +1,25 @@
+﻿swiftApp.controller('BookingController', function ($scope) {
+    $scope.hasApiError = false;
+    $scope.hasApiResult = false;
+
+    $scope.newPerson = {
+        Name: null,
+        PrimaryPhone: null,
+        Address: null
+    };
+
+    $scope.clientsList = [
+        {
+            Name: 'Test Name',
+            PrimaryPhone: '0400 123 456',
+            PickupAddress: '1 Bourke St, Melbourne 3000',
+            DeliveryAddress: '2 Bourke St, Melbourne 3000'
+        },
+        {
+            Name: 'Test Name',
+            PrimaryPhone: '0400 123 456',
+            PickupAddress: '1 Bourke St, Melbourne 3000',
+            DeliveryAddress: '2 Bourke St, Melbourne 3000'
+        }
+    ];
+});
